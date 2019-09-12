8fccb7f0f7581bfed8238841b852b17c714aece3
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-09-11 20:44:21 -0600

    CRUD Update complete
    Final value

--- ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx
+++ ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx
@@ -4,6 +4,22 @@
     <h1>Review Basic CRUD</h1>
 
 
+<div class="row">
+    <div class="col-sm-offset-3">
 
+<asp:Label ID="label" runat="server" Text="Select an Artist to view  albums"> &nbsp;&nbsp;
+
+    <asp:DropDownList ID="ArtistList" runat="server"></asp:DropDownList>
+
+</asp:Label>
+
+
+
+
+
+
+
+    </div>
+</div>
 
 </asp:Content>
--- ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx.cs
+++ ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx.cs
@@ -5,12 +5,36 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+#region Additional NameSpaces
+using ChinookSystem.BLL;
+using ChinookSystem.Data.Entities;
+#endregion
+
 namespace WebApp.SamplePages
 {
     public partial class FilterSearchCRUD : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                BindArtistList();
+            }                                                         
+          
+        }
+
+        protected void BindArtistList()
+        {
+
+            ArtistController sysmgr = new ArtistController();
+            List<Artist> info = sysmgr.Artist_List();
+            //descending order flip x and y
+            info.Sort((x, y) => x.Name.CompareTo(y.Name));
+            ArtistList.DataSource = info;
+            ArtistList.DataTextField = nameof(Artist.Name);
+            ArtistList.DataValueField = nameof(Artist.ArtistId);
+            ArtistList.DataBind();
+            ArtistList.Items.Insert(0,"Select........");
 
         }
     }
