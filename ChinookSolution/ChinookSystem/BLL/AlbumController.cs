e69d6f18355c8e7100f81c39a3e16b7fffddbcab
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-09-11 19:38:56 -0600

    Complete back end of webapp setup

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Name Spaces
+
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    class AlbumController
+    {
+
+        public List<Album> Album_List()
+        {    // using clause starts a transaction Context is the current instance of chinook
+            using (var context = new ChinookContext())
+            {//Context is the current instance of chinook and you can get a property back
+                return context.Albums.ToList();
+            }
+        }
+
+
+        public Album Album_Get(int albumid)
+        {
+            // using clause starts a transaction Context is the current instance of chinook
+            using (var context = new ChinookContext())
+            {//Context is the current instance of chinook and you can get a property back
+                return context.Albums.Find(albumid);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Name Spaces
+
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+   public class ArtistController
+    {
+      
+        public List<Artist> Artist_List()
+        {    // using clause starts a transaction Context is the current instance of chinook
+            using (var context = new ChinookContext())
+            {//Context is the current instance of chinook and you can get a property back
+                return context.Artists.ToList();
+            }
+        }
+
+
+        public Artist Artist_Get(int artistid)
+        {
+            // using clause starts a transaction Context is the current instance of chinook
+            using (var context = new ChinookContext())
+            {//Context is the current instance of chinook and you can get a property back
+                return context.Artists.Find(artistid);
+            }
+        }
+
+    }
+}
