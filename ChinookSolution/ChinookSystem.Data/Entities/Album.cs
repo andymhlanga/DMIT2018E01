cd6a7270d88a80af4aeb21d862a8ffdccb7a2ee7
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-09-20 19:47:48 -0600

    REE Updated Setup

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,35 @@
+namespace ChinookSystem.Data.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Album
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Album()
+        {
+            Tracks = new HashSet<Track>();
+        }
+
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage ="Album Title is required")]
+        [StringLength(160, ErrorMessage = "Album name is limited to a 160 characters")]
+        public string Title { get; set; }
+
+        public int ArtistId { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "Release label  is limited to a 50 characters")]
+        public string ReleaseLabel { get; set; }
+
+        public virtual Artist Artist { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
